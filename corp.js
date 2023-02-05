c2ee5388e03b518e44ff63a8841c022705a36a76
T.J. Eckman <jeek@jeek.net>
2023-02-05 18:25:46 -0500

    Fixing start corp waiting issue introduced in refactoring.

--- corp.js
+++ corp.js
@@ -13,21 +13,21 @@ export async function main(ns) {
         await (new Update(ns, "corp.js", repo)).DownloadAndBuild();
         let filteredArgs = ns.args;
         filteredArgs.remove("--update");
-        if (0 == ns.run("corp.js",1,...filteredArgs)) {
-            ns.spawn("corp.js",1,...filteredArgs);
+        if (0 == ns.run("corp.js", 1, ...filteredArgs)) {
+            ns.spawn("corp.js", 1, ...filteredArgs);
         }
     }
     let settings = {};
-    await ns.asleep(1000);
-    while (Corp.started == false) {
-        ns.toast("Corporation not started yet.");
-        await ns.asleep(60000);
-    }
     if (cmdlineargs['scam']) {
         settings.name = "TurboScam Ltd."
         settings['scam'] = true;
         let Corp = new Corporation(ns, settings);
         Corp.Start();
+        await ns.asleep(1000);
+        while (Corp.started == false) {
+            ns.toast("Corporation not started yet.");
+            await ns.asleep(60000);
+        }
         if (Corp.round == 1)
             await Corp.StartDivision("Software", { "scam": true })
         if (Corp.round == 2)
@@ -43,6 +43,11 @@ export async function main(ns) {
         settings.baseOffers = [210e9, 5e15, 800e15, 500e18];
         let Corp = new Corporation(ns, settings);
         Corp.Start();
+        await ns.asleep(1000);
+        while (Corp.started == false) {
+            ns.toast("Corporation not started yet.");
+            await ns.asleep(60000);
+        }
         Corp.StartDivision("Agriculture", { "name": "jeek Heavier Industries" });
         while (Corp.round < 3) {
             await ns.asleep(10000);
